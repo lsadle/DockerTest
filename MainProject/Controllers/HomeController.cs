459dbee6b0b409901490a01d5bf63df25282510e
Lucas Sadle <lucas.sadle@capstonec.com>
2021-11-17 09:53:01 -0600

    Add project files.

---
+++ MainProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLibrary;
+
+namespace MainProject.Controllers
+{
+    [Route("Home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public string Get()
+        {
+            return Constants.HelloWorld;
+        }
+    }
+}
---
+++ MyLibrary/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class Constants
+    {
+        public static string HelloWorld = "Hello World";
+    }
+}
